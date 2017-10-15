8eab855240ac05d01c70eb9f9f4e37985aa5c962
Nichole <nlasater@ucsc.edu>
2017-10-15 12:47:41 -0700

    attempting to locate src and dest boxes
    but doesn't work properly yet - boxes may overlap?

--- src/p2_pathfinder.py
+++ src/p2_pathfinder.py
@@ -1,4 +1,41 @@
-def find_path (source_point, destination_point, mesh):
-    """    Searches for a path from source_point to destination_point through the mesh    Args:        source_point: starting point of the pathfinder        destination_point: the ultimate goal the pathfinder must reach        mesh: pathway constraints the path adheres to
-    Returns:
-        A path (list of points) from source_point to destination_point if exists        A list of boxes explored by the algorithm    """    path = []    boxes = {}    return path, boxes.keys()
+
+def find_path (source_point, destination_point, mesh):
+    """
+    Searches for a path from source_point to destination_point through the mesh
+
+    Args:
+        source_point: starting point of the pathfinder
+        destination_point: the ultimate goal the pathfinder must reach
+        mesh: pathway constraints the path adheres to
+    Returns:
+        A path (list of points) from source_point to destination_point if exists
+        A list of boxes explored by the algorithm
+    """
+    path = []
+    boxes = {}
+    source_box = None;
+    dest_box = None;
+    
+    ## find location of src point
+    for element in mesh['boxes']:
+      if int(element[0]) < source_point[0]:
+        if int(element[1]) < source_point[1]:
+          if int(element[2]) > source_point[0]:
+            if int(element[3]) > source_point[1]:
+              print("We found the src in box: " + str(element))
+              source_box = element
+      print("not the source!")
+      
+    ## find location of dest point
+    for element in mesh['boxes']:
+      if int(element[0]) < destination_point[0]:
+        if int(element[1]) < destination_point[1]:
+          if int(element[2]) > destination_point[0]:
+            if int(element[3]) > destination_point[1]:
+              print("We found the destination_point in box: " + str(element))
+              dest_box = element
+      print("not the destination_point!")
+    
+   
+
+    return path, boxes.keys()

632711c7d87ed634b55c2b7827175993380fe3b3
Duncan Bauer <dbauer@ucsc.edu>
2017-10-16 09:24:00 -0700

    i forgot the .gitignore

--- src/p2_pathfinder.py
+++ src/p2_pathfinder.py
@@ -1,4 +1,6 @@
 
+from heapq import heappop, heappush
+
 def find_path (source_point, destination_point, mesh):
     """
     Searches for a path from source_point to destination_point through the mesh
@@ -15,43 +17,67 @@ def find_path (source_point, destination_point, mesh):
     boxes = {}
     source_box = None;
     dest_box = None;
-    
+
+    print("Destination point: ")
+    print(destination_point)
+
+    print("Source point: ")
+    print(source_point)
+
     ## find location of src and dest point
     for element in mesh['boxes']:
-      if int(element[0]) < source_point[1] and int(element[1]) > source_point[1] and int(element[2]) < source_point[0] and int(element[3]) > source_point[0]:
-        source_box = element
-        print("We found the src in box: " + str(source_box))
+ #     if int(element[0]) < source_point[1] and int(element[1]) > source_point[1] and int(element[2]) < source_point[0] and int(element[3]) > source_point[0]:
+ #       source_box = element
+ #       print("We found the src in box: " + str(source_box))
         
-      if int(element[0]) < destination_point[1] and int(element[1]) > destination_point[1] and int(element[2]) < destination_point[0] and int(element[3]) > destination_point[0]:
-        dest_box = element
-        print("We found the dest in box: " + str(dest_box))
-      
+ #     if int(element[0]) < destination_point[1] and int(element[1]) > destination_point[1] and int(element[2]) < destination_point[0] and int(element[3]) > destination_point[0]:
+ #       dest_box = element
+ #       print("We found the dest in box: " + str(dest_box))    for element in mesh['boxes']:
+
+        if int(element[0]) < source_point[0] and int(element[1]) > source_point[0] and int(element[2]) < source_point[1] and int(element[3]) > source_point[1]:
+            source_box = element
+            print("We found the src in box: " + str(source_box))
+
+        if int(element[0]) < destination_point[0] and int(element[1]) > destination_point[0] and int(element[2]) < destination_point[1] and int(element[3]) > destination_point[1]:
+            dest_box = element
+            print("We found the dest in box: " + str(dest_box))
+
+
     ## implement BFS to see if a possible path exists
     queue = []
     visited = []
-    queue.append(source_box);
-    while(queue):
-      node = queue.pop()
-      if(node == dest_box):
-        print("Path exists!")
-      else:
-        ## show we visited this node
-        visited.append(node)
-        ## find center of box (center isn't good but will work for this test)
-        y = node[1] - node[0]
-        x = node[3] - node[2]
-        print("(x,y): " + str(x) + ',' + str(y))
-        
-        ## now look for neighboors and add to queue
-        for neighboor in mesh['adj'][node]:
-          print(str(neighboor))
-          if neighboor in visited:
-            continue
-          else:
-            queue.append(neighboor)
-      
-      
-    
-    
+    detail_points = {}
+    path = []
 
+    heappush(queue,(0,source_box))
+    while(queue):
+        node = heappop(queue)
+        box = node[1]
+
+        if(box == dest_box):
+            print("Path exists!")
+        else:
+            # show we visited this node
+            visited.append(box)
+
+            if(box == source_box):
+                y = source_point[0]
+                x = source_point[1]
+                detail_points[box] = (x,y)
+
+            # find center of box (center isn't good but will work for this test)
+            else:
+                y = (int(box[1]) + int(box[0])) / 2
+                x = (int(box[3]) + int(box[2])) / 2
+                detail_points[box] = (x,y)
+
+            #print("(x,y): " + str(x) + ',' + str(y))
+
+        # now look for neighboors and add to queue
+        for neighboor in mesh['adj'][box]:
+            print(neighboor)
+            if neighboor not in visited and neighboor not in queue:
+                heappush(queue,(1, neighboor))
+
+    #print(detail_points)
     return path, boxes.keys()

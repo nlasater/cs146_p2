93271fa94520fd110a8c282d8e7b08deedab2bfa
Duncan Bauer <dbauer@ucsc.edu>
2017-10-16 10:22:18 -0700

    dijks

--- src/p2_pathfinder.py
+++ src/p2_pathfinder.py
@@ -1,5 +1,7 @@
 
 from heapq import heappop, heappush
+import math
+
 
 def find_path (source_point, destination_point, mesh):
     """
@@ -36,48 +38,76 @@ def find_path (source_point, destination_point, mesh):
 
         if int(element[0]) < source_point[0] and int(element[1]) > source_point[0] and int(element[2]) < source_point[1] and int(element[3]) > source_point[1]:
             source_box = element
-            print("We found the src in box: " + str(source_box))
+            #print("We found the src in box: " + str(source_box))
 
         if int(element[0]) < destination_point[0] and int(element[1]) > destination_point[0] and int(element[2]) < destination_point[1] and int(element[3]) > destination_point[1]:
             dest_box = element
-            print("We found the dest in box: " + str(dest_box))
+            #print("We found the dest in box: " + str(dest_box))
 
 
-    ## implement BFS to see if a possible path exists
     queue = []
     visited = []
     detail_points = {}
     path = []
 
+    dist = {}
+    prev = {}
+
+    for square in mesh['boxes']:
+        dist[square] = math.inf
+        prev[square] = None
+
+    dist[source_box] = 0
+    detail_points[source_box] = (source_point[1],source_point[0])
+    detail_points[dest_box] = (destination_point[1],destination_point[0])
     heappush(queue,(0,source_box))
+
     while(queue):
         node = heappop(queue)
         box = node[1]
 
-        if(box == dest_box):
+        if box == dest_box:
             print("Path exists!")
         else:
             # show we visited this node
             visited.append(box)
 
-            if(box == source_box):
-                y = source_point[0]
-                x = source_point[1]
-                detail_points[box] = (x,y)
-
-            # find center of box (center isn't good but will work for this test)
-            else:
-                y = (int(box[1]) + int(box[0])) / 2
-                x = (int(box[3]) + int(box[2])) / 2
-                detail_points[box] = (x,y)
-
-            #print("(x,y): " + str(x) + ',' + str(y))
-
         # now look for neighboors and add to queue
         for neighboor in mesh['adj'][box]:
-            print(neighboor)
-            if neighboor not in visited and neighboor not in queue:
-                heappush(queue,(1, neighboor))
+            y = (int(neighboor[1]) + int(neighboor[0])) / 2
+            x = (int(neighboor[3]) + int(neighboor[2])) / 2
+            detail_points[neighboor] = (x,y)
 
-    #print(detail_points)
-    return path, boxes.keys()
+            heuristic = math.sqrt((detail_points[dest_box][1] - detail_points[neighboor][1])**2 + (detail_points[dest_box][0] - detail_points[neighboor][0])**2)
+            alt = math.sqrt((detail_points[source_box][1] - detail_points[neighboor][1])**2 + (detail_points[source_box][0] - detail_points[neighboor][0])**2)
+            if dist[neighboor] > alt + heuristic:
+                if neighboor not in visited and dist[neighboor] == math.inf:
+                    heappush(queue,(alt + heuristic, neighboor))
+                dist[neighboor] = alt
+                prev[neighboor] = box
+
+        #print(len(queue))
+        #print(queue)
+
+    if dist[dest_box] == math.inf:
+        print("No path found")
+        return None
+    else:
+        print("entered?")
+        shortpath = []
+        #oshortpath = []
+        shortpath.append(box)
+        #oshortpath.append(detail_points[box])
+        w = box
+        while dist[w] != 0:
+            print(prev[w])
+            shortpath.append(prev[w])
+            #oshortpath.append(detail_points[w])
+            w = prev[w]
+        shortpath.reverse()
+        #oshortpath.reverse()
+        print(shortpath)
+        print()
+        #print(oshortpath)
+        print()
+        return shortpath, visited
